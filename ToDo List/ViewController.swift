e126bca32d742781aca89957e8523f6417afe9b7
Lauren Simon <laurenalessandra@laurens-macbook-pro-2.local>
2018-02-19 01:27:44 -0500

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Lauren Simon on 2/19/18.
+//  Copyright © 2018 Simon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
